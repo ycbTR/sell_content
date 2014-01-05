8b24458d056f8924c05e16482353ff4780af58d4
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2014-01-05 18:22:02 +0200

    fixes

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,9 @@
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
+Paperclip.interpolates(:s3_eu_url) do |att, style|
+  "#{att.s3_protocol}://s3-eu-west-1.amazonaws.com/#{att.bucket_name}/#{att.path(style)}"
+end
 
 Paperclip.interpolates :filename do |attachment, style|
   attachment.instance.attachment_file_name
